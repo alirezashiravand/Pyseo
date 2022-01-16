247fef92149b8bd0fb90a0838874835b633d47f1
alirezashiravand <31159211+alirezashiravand@users.noreply.github.com>
2022-01-16 18:50:46 +0330

    batch-in-loop-for-under-100-request

--- indexRequest/index-api-batch.py
+++ indexRequest/index-api-batch.py
@@ -3,7 +3,7 @@ import httplib2
 
 SCOPES = [ "https://www.googleapis.com/auth/indexing" ]
 
-ENDPOINT = "https://indexing.googleapis.com/batch"
+ENDPOINT = "https://indexing.googleapis.com/v3/urlNotifications:publish"
 
 # service_account_file.json is the private key that you created for your service account.
 
@@ -11,27 +11,21 @@ JSON_KEY_FILE = "./secret.json"
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_KEY_FILE, scopes=SCOPES)
 
-http = credentials.authorize(httplib2.Http())
+http = credentials.authorize(httplib2.Http())  
 
-content = """{
+with open('urllist.txt') as file:
+   url_list=[line.rstrip() for line in file]
 
-  "url": "https://darooplus.com",
-
+for index in url_list : 
+  content="""{
+  "url": "%s",
   "type": "URL_UPDATED"
-
-}"""
-
-
-response, content = http.request(ENDPOINT, method="POST", body=content)
-print(response)
-print(content)
-
-returned_response=response
-
-
-# print(response.status)
-# request_date=response.date
-if returned_response.status==200:
-  print(f"indexing requested successfull")
-else:
-  print(f"there is a problem with request")
+  } 
+  """%(index)
+  response, content = http.request(ENDPOINT, method="POST", body=content)
+  returned_response=response
+  
+  if returned_response.status==200:
+    print(f"indexing requested successfull" + str(returned_response.date))
+  else:
+    print(f"there is a problem with request"+"    "+str(returned_response))
